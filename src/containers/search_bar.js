d01f73f28dce3c6db0297e695a55ba05afefde16
athio92 <anthony_thio@utexas.edu>
2017-11-05 17:24:55 +0700

    sparklines added

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,27 +1,40 @@
 import React, {Component} from 'react';
-
+//SearchBar need to Connect to Redux so it can dispatch actionCreators
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
 //we will control the <input /> value with COMPONENT state (not Redux state)
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
   constructor(props){
     super(props);
     this.state={ term:"" };
-    // this.onInputChange = this.onInputChange.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event){
     this.setState({term:event.target.value});
   }
 
+  onFormSubmit(event){
+    //prevent browser default behavior of submitting form & refreshing page  
+    event.preventDefault();
+    //call fetchWeather from SearchBar props, which have been wrapped w. 'dispatch'
+    this.props.fetchWeather(this.state.term) 
+    //clear SearchBox input text
+    this.setState({term:''});
+  }
+
   render(){
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
-          onChange={(event) => this.onInputChange(event)}
+          onChange={this.onInputChange}
         />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary"> Submit!</button>
@@ -30,3 +43,11 @@ export default class SearchBar extends Component{
     )
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather},dispatch);
+}
+
+//we don't need mapStateToProps because SearchBar doesn't have
+//any children, so it doesn't need to pass any state as props.
+export default connect(null,mapDispatchToProps)(SearchBar);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+//for redux-promise to work,the Promise in Actions object have to be wrapped
+//inside 'payload' property, Ex: {type="AN_ACTION_TYPE", payload:aPromiseObject}.
+//So redux-promise stops the action entirely, and once the Promise is resolved,
+//it dispatches a new action with payload : (result of the promise).
+import ReduxPromise from 'redux-promise'; 
+
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//note: applyMiddleware(middleWare) returns the following:
+//return function (createStore) {
+//     return function (reducer, preloadedState, enhancer) {
+//        var store = createStore(reducer, preloadedState, enhancer);
+//        (more code)
+//     }
+// }  
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
